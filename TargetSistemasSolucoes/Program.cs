8594491d01e3e505a2cad7f0a87c3a335e11240e
Ellen Rocha <ellen_rochafarias@hotmail.com>
2023-03-01 14:10:33 -0300

    Solução do exc 02

---
+++ TargetSistemasSolucoes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TargetSistemasSolucoes // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        public static bool verificaFibonacci(int num)
+        {
+            int anterior1 = 0;
+            int anterior2 = 1;
+            int valor = 0;
+            if(num == anterior1 || num == anterior2) { return true; }
+            while (valor <= num)
+            {
+                valor = anterior1 + anterior2;
+                anterior1 = anterior2;
+                anterior2 = valor;
+                if(valor == num) { return true; }
+            }
+            return false;
+
+            
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número:");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine(verificaFibonacci(num) ? "Pertence a sequência de Fibonacci" : " Não pertence a sequência de Fibonacci");
+            Console.ReadKey();
+            
+
+
+        }
+    }
+}
